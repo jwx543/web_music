275ad2945c672667a6d53a0aadd3319ad7e462b0
jwx543 <3075783316@qq.com>
2022-02-10 15:52:29 +0800

    first
    none

---
+++ server/src/test/java/com/agile/server/ServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.agile.server;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
