8d0f93b97db02051919c84326c905c5a9b6d010c
jwx543 <3075783316@qq.com>
2022-02-10 16:08:06 +0800

    upload

--- server/src/main/java/com/agile/server/ServerApplication.java
+++ server/src/main/java/com/agile/server/ServerApplication.java
@@ -1,9 +1,11 @@
 package com.agile.server;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.agile.server.dao")
 public class ServerApplication {
 
     public static void main(String[] args) {
---
+++ server/src/main/java/com/agile/server/service/AdminService.java
@@ -0,0 +1,6 @@
+package com.agile.server.service;
+
+public interface AdminService {
+
+    boolean veritypasswd(String name, String password);
+}
