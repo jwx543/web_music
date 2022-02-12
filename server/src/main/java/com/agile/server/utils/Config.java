21aef032300d5b2e2dbe3fbc0ee87b40fcc67127
jwx543 <3075783316@qq.com>
2022-02-12 21:38:59 +0800

    upload

---
+++ server/src/main/java/com/agile/server/utils/Config.java
@@ -0,0 +1,16 @@
+package com.agile.server.utils;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class Config implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowCredentials(true)
+                .allowedMethods("*")
+                .allowedOrigins("*");
+    }
+}
