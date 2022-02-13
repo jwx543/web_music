0815cc5c110c3d1c27b35e95676758a727b569ca
jwx543 <3075783316@qq.com>
2022-02-13 09:58:05 +0800

    change

--- server/src/main/java/com/agile/server/controller/CollectionController.java
+++ server/src/main/java/com/agile/server/controller/CollectionController.java
@@ -1,4 +1,9 @@
 package com.agile.server.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@Controller
 public class CollectionController {
 }
