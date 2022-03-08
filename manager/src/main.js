2c7855b42b88b7a1ec371afb50e0072555910b16
jwx543 <3075783316@qq.com>
2022-03-08 16:53:08 +0800

    upload

--- manager/src/main.js
+++ manager/src/main.js
@@ -1,4 +1,22 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import Vue from 'vue'
+import Vuex from 'vuex'
+import VCharts from 'v-charts'
+import Element from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
 
-createApp(App).mount('#app')
+
+import App from './App.vue'
+import router from './router'
+import store from './store/index'
+
+
+Vue.use(Vuex)
+Vue.use(VCharts)
+Vue.use(Element)
+
+new Vue({
+    el: '#app',
+    router: router,
+    store: new Vuex.Store(store),
+    render: h => h(App)
+})
